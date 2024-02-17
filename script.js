8a58a76d3cccafc4deba82576b7de6deaf5a1be6
pablosga <pablosgabussi123@gmail.com>
2024-02-17 15:52:01 -0300

    Añade los resultados para que sean visibles en el html
    Ya tiene todas las funcionalidades, salvo la de %

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let num1 = "";
 let num2 = "";
 let operadorBas= "";
 
+let lineaSecundaria = document.querySelector("h2");
+let lineaPrincipal = document.querySelector("h1");
+
+
 function obtieneNum1(num) {
     num1 += num;
     return num1;
@@ -55,32 +59,36 @@ function obtieneResultado() {
 
 let botonNumeros = Array.from(document.querySelectorAll(".numero"));
 botonNumeros.forEach((numero) => numero.addEventListener("click", (e) => {
-    if (!operadorBas) console.log(obtieneNum1(e.target.textContent));
-    else console.log(obtieneNum2(e.target.textContent));
+    if (!operadorBas) lineaPrincipal.textContent = (obtieneNum1(e.target.textContent));
+    else lineaPrincipal.textContent = (obtieneNum2(e.target.textContent));
 }));    
 
 let operadoresBasicos = Array.from(document.querySelectorAll(".basicos"));
 operadoresBasicos.forEach((operador) => operador.addEventListener("click", (e) => {
     operadorBas = e.target.textContent;
-    console.log(e.target.textContent)}));
+    lineaSecundaria.textContent = lineaPrincipal.textContent + (e.target.textContent);
+    lineaPrincipal.textContent = "";
+}));
 
 let operadorIgual = document.querySelector(".igual");
 operadorIgual.addEventListener("click", () => {
-    if (!num1) console.log("");
-    else if (num1 && !num2) console.log(num1);
-    else console.log(obtieneResultado());
+    if (!num1) lineaPrincipal.textContent = ("");
+    else if (num1 && !num2) lineaPrincipal.textContent = (num1);
+    else {
+        lineaPrincipal.textContent = (obtieneResultado());
+        lineaSecundaria.textContent = "";
+    };
 });
 
 let operadorBorrar = document.querySelector(".borrar");
 operadorBorrar.addEventListener("click", () => {
-    if (num2) {
-        num2 = num2.slice(0, -1);
-        console.log(num2);
-    } else if (operadorBas) {
-        operadorBas = "";
-    } else {
+    if (lineaPrincipal.textContent === num1) {
         num1 = num1.slice(0, -1);
-        console.log(num1);
+        lineaPrincipal.textContent = num1;
+    }
+    else if (lineaPrincipal.textContent === num2) {
+        num2 = num2.slice(0, -1);
+        lineaPrincipal.textContent = num2;
     }
 });
 
@@ -89,7 +97,7 @@ operadorLimpiar.addEventListener("click", () => {
     num1 = "";
     num2 = "";
     operadorBas = "";
-    console.log("");
+    lineaPrincipal.textContent = lineaSecundaria.textContent = ("");
 });
 
 let operadorPorcentaje = document.querySelector(".porcentaje");
@@ -110,10 +118,41 @@ let operadorDecimal = document.querySelector(".decimal");
 operadorDecimal.addEventListener("click", () => {
     if (num2 && num2.indexOf(".") == -1) {
         num2 += ".";
-        console.log(num2);
+        lineaPrincipal.textContent = (num2);
     } else if (num1 && num1.indexOf(".") == -1) {
         num1 += ".";
+        lineaPrincipal.textContent = (num1);
+    }
+});
+
+
+/* copia
+
+let operadorBorrar = document.querySelector(".borrar");
+operadorBorrar.addEventListener("click", () => {
+    if (num2) {
+        num2 = num2.slice(0, -1);
+        console.log(num2);
+    } else if (operadorBas) {
+        operadorBas = "";
+    } else {
+        num1 = num1.slice(0, -1);
         console.log(num1);
     }
 });
 
+let operadorPorcentaje = document.querySelector(".porcentaje");
+operadorPorcentaje.addEventListener("click", () => {
+    if (num2 && (operadorBas == "/" || operadorBas == "*")) {
+        num2 = num2 / 100;
+        num2 = String(num2); 
+    } else if (num2 && (operadorBas == "+" || operadorBas == "-")) {
+        num2 = parseFloat(num1) * num2 / 100;
+        num2 = String(num2); 
+    } else if (!operadorBas && num1) {
+        num1 = num1 / 100;
+        num1 = String(num1);
+    }
+})
+
+*/
